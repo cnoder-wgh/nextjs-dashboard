c15c891b500f38d3ce1a8b1f08a983db27b53364
p_ghuawu <p_ghuawu@tencent.com>
2024-06-12 14:40:21 +0800

    feat: add warp data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,7 +14,6 @@ import { unstable_cache as noStore } from "next/cache";
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
@@ -36,6 +35,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -56,6 +56,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
